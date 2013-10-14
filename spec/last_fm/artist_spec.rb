0651dcbca2523480ab2cf0e9e85cb6d5e491d6a5
Richard Davis <rdavis.bacs@gmail.com>
2013-10-14 18:25:07 -0400

    hide api key in cassettes

--- spec/last_fm/artist_spec.rb
+++ spec/last_fm/artist_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe LastFM::Artist do
+describe LastFM::Artist, :vcr do
   context 'searching' do
     it 'returns a list of relevant artists' do
       artist_name = 'Aphex Twin'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,17 @@
 require 'rspec'
+require 'vcr'
 require_relative '../lib/last_fm'
+
 LastFM.api_key = ENV['lastfm_api_key']
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+  c.default_cassette_options = { record: :new_episodes }
+  c.filter_sensitive_data('<LASTFM API KEY>') { LastFM.api_key }
+end
+
+RSpec.configure do |c|
+  c.treat_symbols_as_metadata_keys_with_true_values = true
+end
